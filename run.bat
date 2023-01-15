d3934311bd5d86ddd3622a6cfe2839ba0510f34a
venky <babburivenkat47@gmail.com>
2023-01-15 17:52:26 +0530

    first commit

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\venky\eclipse-workspace\ecommerce
+mvn test
---
+++ src/test/java/testcases/TC_002_LoginTest.java
@@ -0,0 +1,54 @@
+package testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+
+
+import pageobject.HomePage;
+import pageobject.LoginPage;
+import pageobject.MyAccountPage;
+import testbase.BaseClass;
+
+
+public class TC_002_LoginTest extends BaseClass
+{
+	@Test(groups= {"Sanity","Master"}) //Step8 groups added
+	public void test_Login()
+	{
+		logger.info("Starting TC_002_LoginTest");
+		
+		try
+		{				
+			HomePage hp=new HomePage(driver);
+			
+			hp.clickMyAccount();
+						
+			hp.clickLogin();
+			
+			LoginPage lp=new LoginPage(driver);
+			
+			lp.setEmail(rb.getString("email")); // valid email, get it from properties file
+			
+			lp.setPassword(rb.getString("pwd")); // valid password, get it from properties file
+			
+			lp.clickLogin();
+			
+			MyAccountPage macc=new MyAccountPage(driver);
+			
+			boolean targetpage=macc.isMyAccountPageExists();
+						
+			Assert.assertEquals(targetpage, true);
+			
+		}	
+		catch(Exception e)
+		{
+			Assert.fail();
+		}
+		logger.info(" Finished TC_002_LoginTest");
+		
+	}
+	
+	
+}
